dd742ca7a9962a38ea9364c3cd7449b05632aa6b
mick <alex@gcoder.io>
2023-02-06 19:04:27 +0800

    aa

--- app/src/main/java/cn/mick/app/arcblocktest/api/ApiManager.kt
+++ app/src/main/java/cn/mick/app/arcblocktest/api/ApiManager.kt
@@ -15,7 +15,7 @@ const val BASE_URL = "https://arcblockio.cn/"
 
 var StringBuilder.lastChar: Char
     get() = get(length - 1)
-    set(value: Char) {
+    set(value) {
         this.setCharAt(length-1, value)
     }
 
